565d9126e1f55cf65d158ce1bf943a9b5cab4edf
sujatha <jhanavi.bhushan@gmail.com>
2020-06-02 16:18:05 -0400

    Created latest version portfolio

---
+++ script.js
@@ -0,0 +1,54 @@
+const filterButtons = document.querySelector("#filter-btns").children;
+const items=document.querySelector(".portfolio-gallery").children;
+
+// console.log(filterButtons)
+
+for(let i=0; i<filterButtons.length; i++){
+    filterButtons[i].addEventListener("click",function(){
+        for(let j=0; j<filterButtons.length; j++){
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target=this.getAttribute("data-target")
+
+        for(let k=0; k<items.length; k++){
+            items[k].style.display="none";
+            if(target==items[k].getAttribute("data-id"))
+            {
+                items[k].style.display="block";
+            }
+            if(target=="all"){
+                items[k].style.display="block";
+            }
+        }
+    })
+}
+
+//header fixed
+
+window.onscroll=function(){
+    const docScrollTop = document.documentElement.scrollTop;
+
+    if(window.innerWidth > 991){
+        if (docScrollTop > 100){
+            document.querySelector("header").classList.add("fixed")
+        }
+        else{
+            document.querySelector("header").classList.remove("fixed")
+        }
+    }
+}
+
+// navbar links
+
+const navbar= document.querySelector(".navbar");
+    a=navbar.querySelectorAll("a");
+
+    a.forEach(function(element){
+        element.addEventListener("click", function(){
+            for(let i=0; i<a.length; i++){
+                a[i].classList.remove("active");
+            }
+            this.classList.add("active")
+        })
+    })
